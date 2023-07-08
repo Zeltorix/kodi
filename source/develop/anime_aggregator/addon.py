077d8f60b48808d3aa261e2de6bcdd0b40ee77cb
ZelToRiX <61716339+Zeltorix@users.noreply.github.com>
2023-07-08 06:45:05 +0300

    правки

--- source/develop/anime_aggregator/addon.py
+++ source/develop/anime_aggregator/addon.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Module: addon
 # Author: Zeltorix
-# Created on: 2023.05.26
+# Created on: 2023.04.19
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 """
 Плагин для KODI 19.x "Matrix" и выше.
--- source/develop/anime_aggregator/resources/__init__.py
+++ source/develop/anime_aggregator/resources/__init__.py
@@ -1,1 +1,5 @@
 # -*- coding: utf-8 -*-
+# Module: lib.__init__
+# Author: Zeltorix
+# Created on: 2023.04.19
+# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
